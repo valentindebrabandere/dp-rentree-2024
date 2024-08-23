9ec3b2e4dce4078d3a6558c03098403d086f1adf
Valentin de Brabandère <valentindebrabandere@gmail.com>
2024-08-23 15:29:41 +0200

    path corrected

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/pop-corn-show",
+  outDir: "./pop-corn-show",
 
+  // Exemple : Exiger une barre oblique finale pendant le développement
+  trailingSlash: "always",
 });
