8e90d3718d1d40076419dbb55f9e0feca57aae8a
Valentin de Brabandère <valentindebrabandere@gmail.com>
2024-08-26 15:58:12 +0200

    before new content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
+const baseUrl = "/ozuhfrhqbodsc44pfjfsens44indfqznrpi4094902";
+
 // https://astro.build/config
 export default defineConfig({
-  base: "/pop-corn-show",
-  outDir: "./pop-corn-show",
+  base: baseUrl,
+  outDir: "." + baseUrl,
   publicDir: "./public",
 
   // Exemple : Exiger une barre oblique finale pendant le développement
